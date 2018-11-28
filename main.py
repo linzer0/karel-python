46352d2ba776034c95a3b6b058a1afbc56a0e1c4
LinarKhil <zzlinarzz@gmail.com>
2018-11-29 00:43:42 +0500

    New button & basic move realisation

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from tkinter import *
+import const
+import Karel
 from map_generator import *
-from tkinter.filedialog import askopenfilename
 
 window = Tk();
 
@@ -12,26 +13,38 @@ def gui_init(width=1600, height=1400):
     window.title("Python Karel")
     window.geometry("%dx%d+%d+%d" % (width, height, x, y))
 
+def get_map_info():
+    return map_information;
+
+
+def render_object(object_type, column, row):
+    color = 'white'
+    if object_type == '#':
+        color = 'black'
+    if object_type == '+':
+        color='green'
+    if object_type == 'K':
+        color = 'red'
+    coordx = column * 50
+    coordy = row * 50
+    if object_type == 'K':
+        c.create_rectangle(coordx + 5, coordy + 5, coordx + 45, coordy + 45, fill=color)
+    else:
+        c.create_rectangle(coordx, coordy, coordx + 50, coordy + 50, fill=color)
+
+    
 
 def render_map():
     map_info = generate_map(open_file())
+    map_information = map_info;
     height = len(map_info)
+    map_height = height
     width = len(map_info[0])
+    map_width = width
     for column in range(height):
         for row in range(width):
-            coordx = column * 50
-            coordy = row * 50
-            if map_info[column][row] == '#':
-                c.create_rectangle(coordx, coordy, coordx + 50, coordy + 50, fill='black')
-                #WALS RENDERED
-            if map_info[column][row] == 'K':
-                c.create_rectangle(coordx + 5, coordy + 5, coordx + 45, coordy + 45, fill='red')
-                #Karel rendered
-            if map_info[column][row] == '+':
-                c.create_rectangle(coordx, coordy, coordx + 50, coordy + 50, fill='green')
-                #Beeper rendered
-
-
+            render_object(map_info[column][row], column, row)
+            
 def create_grid(event=None):
     w = c.winfo_width() # Get current width of canvas
     h = c.winfo_height() # Get current height of canvas
@@ -53,9 +66,16 @@ gui_init()
 
 def quit():
     window.quit()
+def move():
+    print(const.karel_x, const.karel_y)
+    render_object('K', const.x_karel + 1, const.y_karel)
+    render_object('Z', const.x_karel, const.y_karel)
+    const.x_karel += 1
 
 load = Button(frame_left, text="Load", command=render_map)
+move = Button(frame_left, text="move", command=move)
 exit = Button(frame_left, text="Exit", command=quit)
+move.pack()
 load.pack()
 exit.pack()
 
