d3c4ba74851277e6b8f8f7a48a00e73f7e51213a
LinarKhil <zzlinarzz@gmail.com>
2018-12-17 22:11:44 +0500

    Nope

--- map_generator.py
+++ map_generator.py
@@ -1,11 +1,8 @@
 import json
-import const
 from tkinter.filedialog import askopenfilename
 
 def generate_map(json_data):
     width, height = json_data['mapsize'] # map size set up
-    const.map_width  = width
-    const.map_height = height
     karel_world = [[0 for x in range(width)] for y in range(height)] #world created
     for data in json_data['diamond']:
         x, y = data
@@ -15,9 +12,6 @@ def generate_map(json_data):
         karel_world[x][y] = '#' # 1 = wall
     x, y = json_data['karel']
     karel_world[x][y] = 'K'
-    const.karel_x = x;
-    const.karel_y = y;
-    const.map_information = karel_world
     return karel_world
 
     
@@ -32,6 +26,7 @@ def open_file():
 def print_world(world):
     for i in world:
         print(*i)
+    print('\n')
 
 def test():
     mapp = generate_map(open_file())
