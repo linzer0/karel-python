ece1eb6be9c4a1dac23082cfeb713fb275f1790f
LinarKhil <zzlinarzz@gmail.com>
2018-12-17 22:11:30 +0500

    Nothing important

--- map_creator.py
+++ map_creator.py
@@ -1,9 +1,9 @@
 import json
 map_pr = {
-        "mapsize":((15, 15)),
-        "wall":[(0, 0),(9, 9), (9, 0), (0, 9), (1, 1), (2, 2), (3, 3)],
-        "karel":(5,5),
-        "diamond":[(5, 6), (6, 5)]
+        "mapsize":((5, 5)),
+        "wall":[(1, 1),(0, 1), (0, 2)],
+        "karel":(2,2),
+        "diamond":[(0, 0), (3, 3)]
         };
 with open('01_Task.json', 'w') as out_file:
     json.dump(map_pr, out_file, indent=4)
