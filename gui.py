68e865ae45acd246891fc7a312ed95c74cc5ede4
LinarKhil <zzlinarzz@gmail.com>
2019-01-01 18:26:44 +0500

    Render png fuction added, but doesn't works

--- gui.py
+++ gui.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from PIL import *
 from threading import Thread
 from map_generator import *
 
@@ -22,10 +23,14 @@ class Gui():
         self.add_buttons()
         self.create_canvas()
 
+    def render_png(self, row, column):
+        img = PhotoImage(file="karel.png")
+        self.canvas.create_image(500, 500, anchor=NW, image=img)
+
     def render_object(self, object_type, column, row):
         color = 'white'
-        column -= 1;
-        row -= 1;
+        #column -= 1;
+        #row -= 1;
         if object_type == '#':
             color = 'black'
         if object_type == '+':
@@ -33,11 +38,11 @@ class Gui():
         if object_type == 'K':
             color = 'red'
             self.karel = (column, row)
-        coordx = column * self.size
-        coordy = row * self.size
+        coordx = row * self.size
+        coordy = column * self.size
         if object_type == 'K':
+            #self.render_png(row, column)
             self.canvas.create_rectangle(coordx + 5, coordy + 5, coordx + self.size - 5, coordy + self.size - 5, fill=color)
-            print("RENDERED")
         else:
             self.canvas.create_rectangle(coordx, coordy, coordx + self.size, coordy + self.size, fill=color)
 
@@ -67,13 +72,12 @@ class Gui():
             self.create_canvas();
         for column in range(height):
             for row in range(width):
-                self.render_object(self.world[column][row], column, row)
+                self.render_object(self.world[row][column], row, column)
      
     def create_canvas(self):
         self.size = self.normal_size(self.width, self.height)
         self.canvas = Canvas(self.window, height=self.height * self.size, width=self.width * self.size, bg='white')
         self.canvas.pack(side=TOP, expand=2)
-        self.canvas.pack(expand=2)
         self.canvas.bind('<Configure>', self.create_grid)
 
     def quit(self):
