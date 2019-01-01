585db3db92d963722b3b49b1862596ad88e8b8c9
LinarKhil <zzlinarzz@gmail.com>
2019-01-01 18:27:17 +0500

    Nothing important

--- Task.py
+++ Task.py
@@ -2,9 +2,8 @@ from Karel import *
 
 
 x = Robot()
-x.move()
-x.move()
-x.move()
-x.move()
+x.move();
+x.move();
 x.move()
 
+x.wait()
--- map_creator.py
+++ map_creator.py
@@ -5,7 +5,7 @@ def start():
             "mapsize":((10, 10)),
             "wall":[],
             "karel":(5,1),
-            "diamond":[(0, 0), (3, 3)]
+            "diamond":[(5, 2), (3, 3)]
             };
     with open('01_Task.json', 'w') as out_file:
         json.dump(map_pr, out_file, indent=4)
