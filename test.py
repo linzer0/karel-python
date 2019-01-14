e8dea3f047d8a3f0c6ca63d5afa6a63ade321a1d
LinarKhil <zzlinarzz@gmail.com>
2019-01-15 00:34:37 +0500

    This file created and deleted :)

--- test.py
+++
@@ -1,9 +0,0 @@
-from tkinter import *
-
-window = Tk()
-frame = Frame(window, width=850, height=850, bd=105)
-frame.pack()
-canvas = Canvas(frame, bg='pink', width=50, height=50)
-canvas.pack()
-
-window.mainloop()
